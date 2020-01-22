e325c16c5ecebd0b9a64bd446115ec193dea8545
Deandra Fogel <Deandra.S.Fogel@ou.edu>
2020-01-22 12:34:09 -0600

    Created the in class examples solution and the project for variables

---
+++ inClassExamples/VariablesDF/Program.cs
@@ -0,0 +1,16 @@
+﻿//Deandra Fogel
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
