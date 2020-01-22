f6a2f8b0135ad5ae421e510a8e288df5db5bbb92
Deandra Fogel <Deandra.S.Fogel@ou.edu>
2020-01-22 13:09:01 -0600

    sfhbeut

--- inClassExamples/VariablesDF/Program.cs
+++ inClassExamples/VariablesDF/Program.cs
@@ -36,6 +36,24 @@ namespace VariablesDF
 
             //Make it dynamic
 
+            Console.WriteLine("Please enter your first name: >>");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name: >>");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Please enter your favorite number: >>");
+            FavNum = Convert.ToInt32(FavNum);
+            //favnum = Convert.ToInt32(Console.ReadLine());
+            fullName = FirstName + " " + lastName;
+            crazyNumber = 7.76767 * FavNum;
+
+
+
+            Console.WriteLine(fullName + "has the favorite number of" + FavNum
+                + "\n which equates to the number of" + crazyNumber);
+
+
+
             Console.ReadKey();
         }
     }
