df7abd83df4bc08804e4c9c1372cdfa96cde79a3
Deandra Fogel <Deandra.S.Fogel@ou.edu>
2020-01-22 12:54:13 -0600

    Created variables for firstname, lastname, favnum. Also dervied crazynumber

--- inClassExamples/VariablesDF/Program.cs
+++ inClassExamples/VariablesDF/Program.cs
@@ -11,6 +11,32 @@ namespace VariablesDF
     {
         static void Main(string[] args)
         {
+            //Ceate a string named FirstName
+            string FirstName;
+
+
+
+
+
+            //Assign a value to FirstName (Deandra)
+            FirstName = "Deandra";
+
+            //Create an int named FavNum and assign your favorite number to it
+            int FavNum = 3;
+
+
+            double crazyNumber = 7.76767 * FavNum;
+
+            string lastName = "Fogel";
+
+            string fullName = FirstName + " " + lastName;
+
+            Console.WriteLine(fullName + "has the favorite number of" + FavNum 
+                + "\n which equates to the number of" + crazyNumber);
+
+            //Make it dynamic
+
+            Console.ReadKey();
         }
     }
 }
