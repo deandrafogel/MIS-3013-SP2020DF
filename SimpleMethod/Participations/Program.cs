9379de77a3afd169d7c9dd355de690b7d65e7f33
Deandra Fogel <Deandra.S.Fogel@ou.edu>
2020-03-23 13:07:31 -0500

    Did Simple Method Participation

---
+++ SimpleMethod/Participations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participations
+{
+    class Program
+    {//Deandra Fogel
+        static void Main(string[] args)
+        {
+            string UserInput;
+            string sound = "";
+            Console.WriteLine("Please pick an animal: Cow, Cat, Dog, Pig");
+            UserInput = Console.ReadLine();
+            
+            
+            sound = Speak(UserInput);
+            Console.WriteLine(sound);
+
+        }
+        static string Speak(string UserInput)
+        
+
+        {
+            if (UserInput == "Cow")
+            {
+                sound = "Moo";
+            }
+            else if (UserInput == "Cat")
+            {
+                sound = "Meow";
+            }
+            else if (UserInput == "Dog")
+            {
+                sound = "Woof";
+            }
+            else if (UserInput == "Pig")
+            {
+                sound = "Oink";
+            }
+            else
+            {
+                sound = "Invalid input. Please try again.";
+            }
+            return sound;
+        }
+    }
+}
