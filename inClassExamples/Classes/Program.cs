6fc6e0d746774e22f7b3b1e8123000dcc3fa6af6
Deandra Fogel <Deandra.S.Fogel@ou.edu>
2020-03-29 15:03:48 -0500

    did most of the Classes example from video online.

---
+++ inClassExamples/Classes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Rectangle> rectangles new List<Rectangle>();
+            double w, l;
+
+            w = 5;
+            l = 6;
+
+            Rectangle rect1 = new Rectangle();
+            rect1.Length = 8;
+            rect1.Width = 10;
+
+            Rectangle rect2 = new Rectangle(w, l);
+
+            Rectangle rect3 = new Rectangle(77, 8.2);
+
+            rectangles.Add(rect1);
+
+            foreach (var rectangle in rectangles)
+            {
+                OutputAreaofRect(rectangle);
+            }
+
+
+
+            Console.ReadKey();
+
+
+        }
+
+        static void OutputAreaofRect(Rectangle r)
+        {
+            Console.WriteLine($"The rectangle with {r.Length} length and {r.Width} width has an area of {r.Area()}");
+
+
+        }
+    }
+}
---
+++ inClassExamples/Classes/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Rectangle
+    {
+        public double Width { get; set;  }
+        
+        public double Length { get; set; }
+
+        private DateTime LastModified;
+
+
+
+        //Default/empty constructor for the Rectangle class thats sets the width and length to zero
+        public Rectangle()
+        {
+            Width = 0;
+            Length = 0;
+            LastModified = DateTime.Now;
+
+        }
+
+        //This is the overloaded constructor that takes the width followed by the length as parameters
+        public Rectangle (double width, double length)
+        {
+            Width = width;
+            Length = length;
+            LastModified = DateTime.Now;
+        }
+
+        //This calculates the area of the rectangle, returns double 
+        public double Area()
+        {
+            double a = Width * Length;
+            return a;
+
+        }
+
+    }
+}
