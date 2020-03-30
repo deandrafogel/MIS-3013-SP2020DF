f6f9057c6c541f072c3abd40632b278609b954f2
Deandra Fogel <Deandra.S.Fogel@ou.edu>
2020-03-30 12:56:04 -0500

    Worked on ClassTextBox participation

---
+++ SimpleMethod/ClassesTextBoxParticipation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTextBoxParticipation
+{
+    class Program
+    {//Author: Deandra Fogel, Worked with Elizabeth Sparks and Riley Carr
+        static void Main(string[] args)
+        {
+            TextBox FirstTextBox = new TextBox();
+            FirstTextBox.BackColor = ConsoleColor.Blue;
+            FirstTextBox.ForeColor = ConsoleColor.Magenta;
+            FirstTextBox.Padding = 3;
+            FirstTextBox.Text = "The sky is blue";
+
+            TextBox SecondTextBox = new TextBox();
+            SecondTextBox.BackColor = ConsoleColor.Red;
+            SecondTextBox.ForeColor = ConsoleColor.Green;
+            SecondTextBox.Padding = 10;
+            SecondTextBox.Text = "Red and green are Christmas colors";
+
+            FirstTextBox.DisplayText();
+            SecondTextBox.DisplayText();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SimpleMethod/ClassesTextBoxParticipation/TextBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTextBoxParticipation
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public string Text { get; set; }
+
+        public TextBox()
+        {
+           
+        }
+        public void DisplayText()
+        {
+            Console.ForegroundColor = this.ForeColor;
+            Console.BackgroundColor = this.BackColor;
+            string TextTyped = new string(' ', this.Padding) + this.Text + new string(' ', this.Padding);
+            Console.WriteLine(TextTyped);
+
+        }
+
+
+
+    }
+}
